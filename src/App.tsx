da53579011b2ba0ef1897e82b650bffc39a792ad
Bruno José <bru.matias100@gmail.com>
2025-04-05 20:12:07 -0300

    feat: initial project

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { useCallback, useState } from 'react';
+
+import './App.css';
+import reactLogo from './assets/react.svg';
+
+function Button(props) {
+  return <button type="button" {...props} />;
+}
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const print = useCallback(() => {
+    setCount((state) => state + 1);
+  }, []);
+
+  print();
+
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button type="button" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
