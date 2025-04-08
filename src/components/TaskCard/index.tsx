9918c905e799dab3bc25e25a2fd8a7525212d057
Bruno José <bru.matias100@gmail.com>
2025-04-08 14:57:16 -0300

    feat: adding accessibility to marking tasks as completed

--- src/components/TaskCard/index.tsx
+++ src/components/TaskCard/index.tsx
@@ -1,4 +1,5 @@
 import { Trash } from '@phosphor-icons/react';
+import { useEffect, useRef } from 'react';
 
 import { ITask } from '@/@types/ITask';
 
@@ -15,15 +16,43 @@ export function TaskCard({
   onMarkingTaskAsCompleted,
   onDeleteTask,
 }: ITaskCard) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    function handleMarkTaskCompletedPressingEnterKey(event: KeyboardEvent) {
+      const inputElement = inputRef.current;
+
+      if (
+        document.activeElement === inputElement &&
+        inputElement?.type === 'checkbox' &&
+        event.key === 'Enter'
+      ) {
+        event.preventDefault();
+        onMarkingTaskAsCompleted(Number(inputElement?.value));
+      }
+    }
+
+    document.addEventListener(
+      'keypress',
+      handleMarkTaskCompletedPressingEnterKey,
+    );
+
+    return () =>
+      document.removeEventListener(
+        'keypress',
+        handleMarkTaskCompletedPressingEnterKey,
+      );
+  }, [task, onMarkingTaskAsCompleted]);
+
   return (
     <div className={styles.task}>
       <div>
         <input
+          value={task.id}
           checked={task.is_completed}
           onChange={() => onMarkingTaskAsCompleted(task.id)}
           type="checkbox"
-          name=""
-          id=""
+          ref={inputRef}
         />
         <p className={task.is_completed ? styles.completed : ''}>
           {task.description}
